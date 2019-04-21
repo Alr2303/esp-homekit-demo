41bc50466cf2cef08e3cbbea1927bf285aa9fc0d
Anand Loorthu Raj <49178448+Alr2303@users.noreply.github.com>
2019-04-22 00:47:12 +0530

    Update fanlight.c

--- examples/fanlight/fanlight.c
+++ examples/fanlight/fanlight.c
@@ -51,29 +51,14 @@ void fan_init() {
     fan_write(fan_on);
 }
 
-//LIGHT IDENTIFY TASK
-void light_identify_task(void *_args) {
+//IDENTIFY TASK
+void identify_task(void *_args) {
     for (int i=0; i<3; i++) {
         for (int j=0; j<2; j++) {
             light_write(true);
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-            light_write(false);
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-        }
-
-        vTaskDelay(250 / portTICK_PERIOD_MS);
-    }
-
-    light_write(light_on);
-
-    vTaskDelete(NULL);
-}
-//FAN IDENTIFY TASK
-void fan_identify_task(void *_args) {
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<2; j++) {
             fan_write(true);
             vTaskDelay(100 / portTICK_PERIOD_MS);
+            light_write(false);
             fan_write(false);
             vTaskDelay(100 / portTICK_PERIOD_MS);
         }
@@ -81,6 +66,7 @@ void fan_identify_task(void *_args) {
         vTaskDelay(250 / portTICK_PERIOD_MS);
     }
 
+    light_write(light_on);
     fan_write(fan_on);
 
     vTaskDelete(NULL);
@@ -89,12 +75,12 @@ void fan_identify_task(void *_args) {
 //IDENTITY LIGHT
 void light_identify(homekit_value_t _value) {
     printf("Light identify\n");
-    xTaskCreate(light_identify_task, "LIGHT identify", 128, NULL, 2, NULL);
+    xTaskCreate(identify_task, "Light identify", 128, NULL, 2, NULL);
 }
 //IDENTITY FAN
 void fan_identify(homekit_value_t _value) {
     printf("Fan identify\n");
-    xTaskCreate(fan_identify_task, "FAN identify", 128, NULL, 2, NULL);
+    xTaskCreate(identify_task, "Fan identify", 128, NULL, 2, NULL);
 }
 
 
@@ -130,6 +116,7 @@ void fan_on_set(homekit_value_t value) {
 
 
 homekit_accessory_t *accessories[] = {
+    //FOR LIGHT
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]){
         HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
             HOMEKIT_CHARACTERISTIC(NAME, "Light"),
@@ -138,15 +125,6 @@ homekit_accessory_t *accessories[] = {
             HOMEKIT_CHARACTERISTIC(MODEL, "LIGHT"),
             HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
             HOMEKIT_CHARACTERISTIC(IDENTIFY, light_identify),
-            NULL
-    HOMEKIT_ACCESSORY(.id=2, .category=homekit_accessory_category_fan, .services=(homekit_service_t*[]){
-        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
-            HOMEKIT_CHARACTERISTIC(NAME, "Fan"),
-            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "ALR"),
-            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "23032000AAAA"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "FAN"),
-            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
-            HOMEKIT_CHARACTERISTIC(IDENTIFY, fan_identify),
             NULL
     
         }),
@@ -159,17 +137,38 @@ homekit_accessory_t *accessories[] = {
                 
                 
         
+            ),
+            NULL
+        }),
+        NULL
+    }),
+    //FOR FAN
+    HOMEKIT_ACCESSORY(.id=2, .category=homekit_accessory_category_fan, .services=(homekit_service_t*[]){
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Fan"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "ALR"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "23032000AAAA"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "FAN"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, fan_identify),
+            NULL
+    
+        }),
         HOMEKIT_SERVICE(FAN, .primary=true, .characteristics=(homekit_characteristic_t*[]){
             HOMEKIT_CHARACTERISTIC(NAME, "Fan"),
             HOMEKIT_CHARACTERISTIC(
                 ON, false,
                 .getter=fan_on_get,
                 .setter=fan_on_set
+                
+                
+        
             ),
             NULL
         }),
         NULL
     }),
+    
     NULL
 };
 
