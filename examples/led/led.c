744bcc9751a08b3123e5efca46b99f6b80e6abdd
Anand Loorthu Raj <49178448+Alr2303@users.noreply.github.com>
2019-07-27 08:10:56 +0530

    Update led.c
    Attempt for two led in one code

--- examples/led/led.c
+++ examples/led/led.c
@@ -23,15 +23,26 @@ static void wifi_init() {
 }
 
 const int led_gpio = 2;
+const int led_gpio1 = 4;
 bool led_on = false;
+bool led_on1 = false;
 
 void led_write(bool on) {
     gpio_write(led_gpio, on ? 0 : 1);
 }
 
+void led_write1(bool on) {
+    gpio_write(led_gpio1, on ? 0 : 1);
+}
+
 void led_init() {
     gpio_enable(led_gpio, GPIO_OUTPUT);
     led_write(led_on);
+}
+
+void led_init1() {
+    gpio_enable(led_gpio1, GPIO_OUTPUT);
+    led_write1(led_on1);
 }
 
 void led_identify_task(void *_args) {
@@ -50,6 +61,26 @@ void led_identify_task(void *_args) {
 
     vTaskDelete(NULL);
 }
+
+
+void led_identify_task(void *_args) {
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            led_write1(true);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            led_write1(false);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+    }
+
+    led_write1(led_on1);
+
+    vTaskDelete(NULL);
+}
+
+
 
 void led_identify(homekit_value_t _value) {
     printf("LED identify\n");
@@ -71,19 +102,40 @@ void led_on_set(homekit_value_t value) {
 }
 
 
+void led_identify(homekit_value_t _value) {
+    printf("LED identify1\n");
+    xTaskCreate(led_identify_task, "LED identify1", 128, NULL, 2, NULL);
+}
+
+homekit_value_t led_on_get1() {
+    return HOMEKIT_BOOL(led_on1);
+}
+
+void led_on_set1(homekit_value_t value) {
+    if (value.format != homekit_format_bool) {
+        printf("Invalid value format: %d\n", value.format);
+        return;
+    }
+
+    led_on1 = value.bool_value;
+    led_write1(led_on1);
+}
+
+
+
 homekit_accessory_t *accessories[] = {
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]){
         HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
-            HOMEKIT_CHARACTERISTIC(NAME, "Sample LED"),
-            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "HaPK"),
-            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "037A2BABF19D"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "MyLED"),
+            HOMEKIT_CHARACTERISTIC(NAME, "Light"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "ALR"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "23031999AAAA"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "Light"),
             HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
             HOMEKIT_CHARACTERISTIC(IDENTIFY, led_identify),
             NULL
         }),
         HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]){
-            HOMEKIT_CHARACTERISTIC(NAME, "Sample LED"),
+            HOMEKIT_CHARACTERISTIC(NAME, "Light"),
             HOMEKIT_CHARACTERISTIC(
                 ON, false,
                 .getter=led_on_get,
@@ -96,9 +148,35 @@ homekit_accessory_t *accessories[] = {
     NULL
 };
 
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=2, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]){
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Light"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "ALR"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "23031999AAAA"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "Light"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, led_identify1),
+            NULL
+        }),
+        HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Light"),
+            HOMEKIT_CHARACTERISTIC(
+                ON, false,
+                .getter=led_on_get1,
+                .setter=led_on_set1
+            ),
+            NULL
+        }),
+        NULL
+    }),
+    NULL
+};
+
 homekit_server_config_t config = {
     .accessories = accessories,
-    .password = "111-11-111"
+    .password = "230-31-999"
 };
 
 void user_init(void) {
